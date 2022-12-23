3fb7cac5f7353c5833b618d77653ad3ba9dffe8c
huanxi6 <104499460+huanxi6@users.noreply.github.com>
2022-12-23 10:04:40 +0300

    Add files via upload

---
+++ index1/index.js
@@ -0,0 +1,16 @@
+flatpickr("#date", {
+    minDate: "today",
+    mode: "range"
+});
+function a() {
+    window.setTimeout("window.location='./404.html'", 800);
+}
+function login_button() {
+    window.setTimeout("window.location='../login/login.html'", 800);
+}
+function c() {
+    window.setTimeout("window.location='./index.html'", 800);
+}
+function X() {
+    window.setTimeout("window.location='../index2/index2.html'", 800);
+}
---
+++ index2/index2.js
@@ -0,0 +1,11 @@
+function a() {
+    window.setTimeout("window.location='../index1/404.html'", 800);
+}
+function login_button() {
+    window.setTimeout("window.location='../login/login.html'", 800);
+}
+function c() {
+    window.setTimeout("window.location='../index1/index.html'", 800);
+}
+document.getElementById("demo").innerHTML = new Date().getMonth()
+document.getElementById("year").innerHTML = new Date().getDate() + 1
